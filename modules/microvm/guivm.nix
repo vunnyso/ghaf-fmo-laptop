814c95edcc898244fdbc3b0fc6b4c80845ed750e
Brian McGillion <bmg.avoin@gmail.com>
2025-04-01 19:56:36 +0400

    chrome and firefox with HW accel
    DO NOT MERGE YET
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/microvm/guivm.nix
+++ modules/microvm/guivm.nix
@@ -6,9 +6,27 @@
 }:
 {
   config = {
-    environment.systemPackages = [
-      pkgs.google-chrome
-      pkgs.libaom
+    ghaf.graphics.nvidia-setup = {
+      enable = true;
+      vaapi.firefox.av1Support = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      (google-chrome.override {
+        commandLineArgs = [
+          # Hardware video encoding on Chrome on Linux.
+          # See chrome://gpu to verify.
+          # Enable H.265 video codec support.
+          # Turn on vulkan support
+          "--enable-features=UseOzonePlatform,VaapiVideoDecoder,VaapiVideoEncoder,WebRtcAllowH265Receive,Vulkan,VaapiIgnoreDriverChecks,DefaultANGLEVulkan,VulkanFromANGLE"
+          "--force-fieldtrials=WebRTC-Video-H26xPacketBuffer/Enabled"
+        ];
+      })
     ];
+
+    programs.firefox = {
+      package = pkgs.firefox-beta;
+      enable = true;
+    };
   };
 }
